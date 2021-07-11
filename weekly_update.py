d59e23b924d1dd87e5861a4c5b5dc7d3ea16cc9f
Aidan <aidanvickars@gmail.com>
2021-07-11 13:41:22 -0700

    removed comments on daily and weekly update

--- weekly_update.py
+++ weekly_update.py
@@ -4,15 +4,15 @@ from DataGenerators.backup_generator import upload_backup, get_new_backup
 
 
 def main():
-    if update_players() == -1:
+    # if update_players() == -1:
+    #     return -1
+    # record_script_execution('update_players')
+    if get_new_backup() == -1:
         return -1
-    record_script_execution('update_players')
-    # if get_new_backup() == -1:
-    #     return -1
-    # record_script_execution('get_new_backup')
-    # if upload_backup() == -1:
-    #     return -1
-    # record_script_execution('upload_backup')
+    record_script_execution('get_new_backup')
+    if upload_backup() == -1:
+        return -1
+    record_script_execution('upload_backup')
 
 
 if __name__ == '__main__':
