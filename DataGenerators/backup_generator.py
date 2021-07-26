c6c1fab44df3cf425b81f3db48937be0db45cb56
Aidan <aidanvickars@gmail.com>
2021-07-26 16:57:39 -0700

    completed initial implementation of player valuation model
    using deep reinforcement learning

--- DataGenerators/backup_generator.py
+++ DataGenerators/backup_generator.py
@@ -29,7 +29,7 @@ def delete_old_backup():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                '../credentials.json', SCOPES)
+                'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
@@ -83,7 +83,7 @@ def upload_backup():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                '../credentials.json', SCOPES)
+                'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
