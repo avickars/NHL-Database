ebe5c924ff0bb006b5fe04641e7cd3be99ec70f1
Aidan <aidanvickars@gmail.com>
2021-08-31 16:09:54 -0700

    small adjustments

--- daily_update.py
+++ daily_update.py
@@ -6,47 +6,46 @@ from DataGenerators.boxscore_generator import get_boxscore
 from DataGenerators.data_to_production import data_to_production
 from ETL.etl_create_gim_model_sequences import create_gim_model_sequences
 from ETL.etl_consolidate_gim_values import get_new_consolidated_gims
-from ETL.etl_predicting_game_outcome import predict_game_outcome
 from ETL.etl_create_gim_values import create_gim_values
 from SQLCode.execute_stored_procedure import execute_proc
 from ETL.etl_game_outcome_prediction import get_game_outcome_predictions
 
 
 def main():
-    # # Downloading data
-    # if get_daily_schedule() == -1:
-    #     return -1
-    # record_script_execution('get_daily_schedule')
-    # if get_live_data() == -1:
-    #     return -1
-    # record_script_execution('get_live_data')
-    # if get_boxscore() == -1:
-    #     return -1
-    # record_script_execution('get_boxscore')
-    # if get_new_players() == -1:
-    #     return -1
-    # record_script_execution('get_new_players')
-    #
-    # # ETLs
-    # if create_gim_model_sequences() == -1:
-    #     return -1
-    # record_script_execution('create_gim_model_sequences')
-    #
-    # if create_gim_values() == -1:
-    #     return -1
-    # record_script_execution('create_gim_values')
-    #
-    # if get_new_consolidated_gims() == -1:
-    #     return -1
-    # record_script_execution('get_new_consolidated_gims')
-    #
-    # if execute_proc("sp_game_prediction_team_stats_view") == -1:
-    #     return -1
-    # record_script_execution('sp_game_prediction_team_stats_view')
-    #
-    # if get_game_outcome_predictions() == -1:
-    #     return -1
-    # record_script_execution('get_game_outcome_predictions')
+    # Downloading data
+    if get_daily_schedule() == -1:
+        return -1
+    record_script_execution('get_daily_schedule')
+    if get_live_data() == -1:
+        return -1
+    record_script_execution('get_live_data')
+    if get_boxscore() == -1:
+        return -1
+    record_script_execution('get_boxscore')
+    if get_new_players() == -1:
+        return -1
+    record_script_execution('get_new_players')
+
+    # ETLs
+    if create_gim_model_sequences() == -1:
+        return -1
+    record_script_execution('create_gim_model_sequences')
+
+    if create_gim_values() == -1:
+        return -1
+    record_script_execution('create_gim_values')
+
+    if get_new_consolidated_gims() == -1:
+        return -1
+    record_script_execution('get_new_consolidated_gims')
+
+    if execute_proc("sp_game_prediction_team_stats_view") == -1:
+        return -1
+    record_script_execution('sp_game_prediction_team_stats_view')
+
+    if get_game_outcome_predictions() == -1:
+        return -1
+    record_script_execution('get_game_outcome_predictions')
 
     # if predict_game_outcome() == -1:
     #     return -1
