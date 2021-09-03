53c5457fbca5640abd95c0339ad9194e70927601
Aidan <aidanvickars@gmail.com>
2021-09-03 08:47:56 -0700

    forgot to uncomment stuff

--- daily_update.py
+++ daily_update.py
@@ -13,36 +13,36 @@ from ETL.etl_game_outcome_prediction import get_game_outcome_predictions
 
 def main():
     # Downloading data
-    # if get_daily_schedule() == -1:
-    #     return -1
-    # record_script_execution('get_daily_schedule')
-    # if get_live_data() == -1:
-    #     return -1
-    # record_script_execution('get_live_data')
-    # if get_boxscore() == -1:
-    #     return -1
-    # record_script_execution('get_boxscore')
-    # if get_new_players() == -1:
-    #     return -1
-    # record_script_execution('get_new_players')
-    #
-    # # ETLs
-    # if create_gim_model_sequences() == -1:
-    #     return -1
-    # record_script_execution('create_gim_model_sequences')
-    #
-    # if create_gim_values() == -1:
-    #     return -1
-    # record_script_execution('create_gim_values')
-    #
-    # if get_new_consolidated_gims() == -1:
-    #     return -1
-    # record_script_execution('get_new_consolidated_gims')
-    #
-    # if execute_proc("sp_game_prediction_team_stats_view") == -1:
-    #     return -1
-    # record_script_execution('sp_game_prediction_team_stats_view')
-    #
+    if get_daily_schedule() == -1:
+        return -1
+    record_script_execution('get_daily_schedule')
+    if get_live_data() == -1:
+        return -1
+    record_script_execution('get_live_data')
+    if get_boxscore() == -1:
+        return -1
+    record_script_execution('get_boxscore')
+    if get_new_players() == -1:
+        return -1
+    record_script_execution('get_new_players')
+
+    # ETLs
+    if create_gim_model_sequences() == -1:
+        return -1
+    record_script_execution('create_gim_model_sequences')
+
+    if create_gim_values() == -1:
+        return -1
+    record_script_execution('create_gim_values')
+
+    if get_new_consolidated_gims() == -1:
+        return -1
+    record_script_execution('get_new_consolidated_gims')
+
+    if execute_proc("sp_game_prediction_team_stats_view") == -1:
+        return -1
+    record_script_execution('sp_game_prediction_team_stats_view')
+
     if get_game_outcome_predictions() == -1:
         return -1
     record_script_execution('get_game_outcome_predictions')
